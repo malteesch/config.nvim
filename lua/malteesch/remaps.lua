64e049249704794cef9a41327996aa4f0361d813
Malte Esch <accounts@malteesch.de>
2023-11-15 20:37:07 +0100

    feat: add basic remaps

---
+++ lua/malteesch/remaps.lua
@@ -0,0 +1,14 @@
+-- GLOBAL
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- WINDOW
+-- 	jump between windows
+vim.keymap.set('n', '<A-j>', '<C-w>j')
+vim.keymap.set('n', '<A-k>', '<C-w>k')
+vim.keymap.set('n', '<A-h>', '<C-w>h')
+vim.keymap.set('n', '<A-l>', '<C-w>l')
+vim.keymap.set('n', '<leader>c', '<C-w>q') -- close window
+vim.keymap.set('n', '<leader>s', '<C-w>s') -- split window
+vim.keymap.set('n', '<leader>v', '<C-w>v') -- split window vertically
+
