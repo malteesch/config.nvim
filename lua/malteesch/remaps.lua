8d8aab726775e20aedb4ecd7e68209a7f8cbbe63
Malte Esch <accounts@malteesch.de>
2023-12-13 16:45:30 +0100

    refactor: extract more code out of init.lua

--- lua/malteesch/remaps.lua
+++ lua/malteesch/remaps.lua
@@ -1,16 +1,28 @@
--- GLOBAL
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
 -- WINDOW
 -- 	jump between windows
 vim.keymap.set('n', '<A-j>', '<C-w>j')
 vim.keymap.set('n', '<A-k>', '<C-w>k')
 vim.keymap.set('n', '<A-h>', '<C-w>h')
 vim.keymap.set('n', '<A-l>', '<C-w>l')
-vim.keymap.set('n', '<leader>c', '<C-w>q') -- close window
+vim.keymap.set('n', '<leader>c', '<cmd>bdelete<CR>') -- close buffer
 vim.keymap.set('n', '<leader>s', '<C-w>s') -- split window
 vim.keymap.set('n', '<leader>v', '<C-w>v') -- split window vertically
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
+
+-- [[ Basic Keymaps ]]
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
