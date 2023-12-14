da9ef7fd350e8f6539b565bbfa4128092a505619
Malte Esch <accounts@malteesch.de>
2023-12-14 19:32:18 +0100

    refactor: finish init file cleanup

--- lua/malteesch/plugins/neotree.lua
+++ lua/malteesch/plugins/neotree.lua
@@ -1,5 +1,3 @@
--- vim.keymap.set('n', '<leader>fo', ':Neotree focus position=float<CR>')
-
 return {
   "nvim-neo-tree/neo-tree.nvim",
   version = "*",
@@ -8,15 +6,14 @@ return {
     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
     "MunifTanjim/nui.nvim",
   },
-  config = function ()
-    require('neo-tree').setup {
-      close_if_last_window = true,
-      window = {
-        position = 'float'
-      },
-      popup_border_style = 'rounded'
-    }
-  end,
+  opts = {
+    close_if_last_window = true,
+    window = {
+      position = 'float'
+    },
+    popup_border_style = 'rounded',
+    enable_diagnostics = true
+  },
   keys = {
     {'<leader>fo', '<cmd>Neotree focus position=float<CR>', 'n', desc = '[F]iletree [o]pen'},
     {'<leader>fc', '<cmd>Neotree focus position=float reveal_file=%<CR>', 'n', desc = '[F]iletree open [c]urrent file'}
