a96d7e4be77b4cfe9769b2dabca7891aa541c020
Malte Esch <accounts@malteesch.de>
2023-12-16 19:37:10 +0100

    feat: add trouble.nvim

--- lua/malteesch/plugins/telescope.lua
+++ lua/malteesch/plugins/telescope.lua
@@ -44,15 +44,15 @@ return {
     }
   },
   keys = {
-    {'<leader>gf', function() require('telescope.builtin').git_files() end,  desc = 'Search [G]it [F]iles' },
+    {'<leader>sgf', function() require('telescope.builtin').git_files() end,  desc = '[S]earch [G]it [F]iles' },
     {'<leader>sf', function() require('telescope.builtin').find_files() end,  desc = '[S]earch [F]iles' },
     {'<leader>sh', function() require('telescope.builtin').help_tags() end,  desc = '[S]earch [H]elp' },
     {'<leader>sw', function() require('telescope.builtin').grep_string() end,  desc = '[S]earch current [W]ord' },
     {'<leader>sg', function() require('telescope.builtin').live_grep() end,  desc = '[S]earch by [G]rep' },
     {'<leader>sd', function() require('telescope.builtin').diagnostics() end,  desc = '[S]earch [D]iagnostics' },
     {'<leader>sr', function() require('telescope.builtin').resume() end,  desc = '[S]earch [R]esume' },
-    {'<leader>?', function() require('telescope.builtin').oldfiles() end, desc = '[?] Find recently opened files' },
-    {'<leader><space>', function() require('telescope.builtin').buffers() end, desc = '[ ] Find existing buffers' },
+    {'<leader>rf', function() require('telescope.builtin').oldfiles() end, desc = '[R]ecent [f]iles' },
+    {'<leader>sb', function() require('telescope.builtin').buffers() end, desc = '[S]earch existing [b]uffers' },
     {'<leader>/', function ()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
