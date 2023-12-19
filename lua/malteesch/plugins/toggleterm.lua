bd84895bc06d9688696f4960ab374b1578d87d13
Malte Esch <accounts@malteesch.de>
2023-12-19 14:32:18 +0100

    feat: add lazygit and btop floating terminals

--- lua/malteesch/plugins/toggleterm.lua
+++ lua/malteesch/plugins/toggleterm.lua
@@ -3,11 +3,32 @@ return {
   version = "*",
   opts = {
     open_mapping = [[<C-\>]],
-    direction = 'float',
+    direction = 'horizontal',
     autochdir = true,
     float_opts = {
       border = 'curved'
     }
-  }
+  },
+  config = function (_, opts)
+    local tt = require('toggleterm')
+    local Terminal = require('toggleterm.terminal').Terminal
+    tt.setup(opts)
+
+    local lazygit = Terminal:new {
+      cmd = 'lazygit',
+      direction = 'float',
+      hidden = true,
+    }
+
+    local btop = Terminal:new {
+      cmd = 'btop',
+      direction = 'float',
+      hidden = true,
+    }
+
+
+    vim.keymap.set('n', '<leader>lg', function () lazygit:toggle() end, { desc = 'Terminal: [l]azy[g]it' })
+    vim.keymap.set('n', '<leader>bt', function () btop:toggle() end, { desc = 'Terminal: [b][t]op' })
+  end
 }
 
