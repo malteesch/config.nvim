fc847d0f9e74bb1d231e2ed21e50b4e43a0ab7ac
Malte Esch <accounts@malteesch.de>
2023-12-19 15:43:05 +0100

    chore: cleanup some whichkey chains

--- lua/malteesch/plugins/harpoon.lua
+++ lua/malteesch/plugins/harpoon.lua
@@ -11,11 +11,11 @@ return {
     local harpoon = require('harpoon')
     harpoon:setup(opts)
 
-    vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = '[H]arpoon [a]ppend file' })
-    vim.keymap.set('n', '<leader>hp', function () harpoon:list():prepend() end, { desc = '[H]arpoon [p]repend file' })
-    vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end, { desc = '[H]arpoon [r]emove file' })
-    vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end, { desc = '[H]arpoon [c]lear list' })
-    vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[H]arpoon [l]ist popup'})
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end, { desc = '[a]ppend file' })
+    vim.keymap.set('n', '<leader>hp', function() harpoon:list():prepend() end, { desc = '[p]repend file' })
+    vim.keymap.set("n", "<leader>hr", function() harpoon:list():remove() end, { desc = '[r]emove file' })
+    vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end, { desc = '[c]lear list' })
+    vim.keymap.set("n", "<leader>hl", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, { desc = '[l]ist popup'})
 
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end, { desc = 'Jump to harpoon item 1' })
     vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end, { desc = 'Jump to harpoon item 2' })
