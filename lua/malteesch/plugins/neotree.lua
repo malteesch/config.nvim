451b39edb0539922614df87a2d6606fe257751e8
Malte Esch <accounts@malteesch.de>
2023-12-21 09:53:29 +0100

    feat: add file following with left file tree

--- lua/malteesch/plugins/neotree.lua
+++ lua/malteesch/plugins/neotree.lua
@@ -10,16 +10,20 @@ return {
   opts = {
     close_if_last_window = true,
     window = {
-      position = 'float'
+      position = 'left'
     },
     popup_border_style = 'rounded',
     enable_diagnostics = true,
     filesystem = {
-      hijack_netrw_behavior = 'open_default'
+      hijack_netrw_behavior = 'open_default',
+      follow_current_file = {
+        enabled = true
+      }
     }
   },
   keys = {
-    {'<leader>fo', '<cmd>Neotree focus position=float<CR>', 'n', desc = '[F]iletree [o]pen'},
-    {'<leader>fc', '<cmd>Neotree focus position=float reveal_file=%<CR>', 'n', desc = '[F]iletree open [c]urrent file'}
+    {'<leader>fo', '<cmd>Neotree focus position=float<CR>', 'n', desc = '[o]pen'},
+    {'<leader>fc', '<cmd>Neotree focus position=float reveal_file=%<CR>', 'n', desc = 'open [c]urrent file'},
+    {'<leader>ff', '<cmd>Neotree toggle position=left<CR>', desc = '[f]ollow current file'}
   }
 }
