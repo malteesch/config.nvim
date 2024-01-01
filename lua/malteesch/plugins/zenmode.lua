14b225ddd0ee6d9a9696e941728410a4c8836d31
Malte Esch <accounts@malteesch.de>
2024-01-01 19:50:24 +0100

    feat: add zen mode with twilight

---
+++ lua/malteesch/plugins/zenmode.lua
@@ -0,0 +1,33 @@
+--- @type LazyPluginSpec
+return {
+  "folke/zen-mode.nvim",
+  --- @type ZenOptions
+  opts = {
+    window = {
+      backdrop = 1,
+      width = 160
+    },
+    plugins = {
+      tmux = { enabled = true }
+    }
+  },
+  dependencies = {
+    {
+      "folke/twilight.nvim",
+      --- @type TwilightOptions
+      opts = {
+        expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+          "constructor_declaration",
+          "function",
+          "method",
+          "method_declaration",
+          "table",
+        },
+      }
+    }
+  },
+  keys = {
+    { "<leader>td", function() require("zen-mode").toggle() end, { desc = "[T]oggle [d]istraction free mode" } }
+  }
+}
+
