0e29401eb5b4d9d9196bf413dd229ec94e6af77d
Malte Esch <accounts@malteesch.de>
2024-01-02 13:41:28 +0100

    feat: add obsidian plugin

---
+++ lua/malteesch/plugins/obsidian.lua
@@ -0,0 +1,35 @@
+--- @type LazyPluginSpec
+return {
+    'epwalsh/obsidian.nvim',
+    version = '*', -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = 'markdown',
+    event = {
+      -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+      "BufReadPre " .. vim.fn.expand('~') .. "/Cerebro/**.md",
+      "BufNewFile " .. vim.fn.expand('~') .. "/Cerebro/**.md",
+    },
+    dependencies = {
+        -- Required.
+        'nvim-lua/plenary.nvim',
+        'hrsh7th/nvim-cmp',
+        'nvim-telescope/telescope.nvim',
+        'nvim-telescope/telescope.nvim',
+        'nvim-treesitter/nvim-treesitter',
+    },
+    opts = {
+        workspaces = {
+            {
+                name = 'cerebro',
+                path = '~/Cerebro',
+            },
+        },
+        follow_url_func = function (url)
+            vim.fn.jobstart({"xdg-open", url})
+        end
+    },
+    config = function (_, opts)
+        require("obsidian").setup(opts)
+        vim.o.conceallevel = 1
+    end
+}
