46f4edad529ee4d8f514aaa6ba3a9830afe8e612
Malte Esch <accounts@malteesch.de>
2024-01-03 16:01:30 +0100

    fix: exclude more filetypes from hardtime

--- lua/malteesch/plugins/hardtime.lua
+++ lua/malteesch/plugins/hardtime.lua
@@ -3,6 +3,6 @@ return {
     'm4xshen/hardtime.nvim',
     dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
     opts = {
-        disabled_filetypes = { 'qf', 'netrw', 'neo-tree', 'lazy', 'mason', 'query' },
+        disabled_filetypes = { 'qf', 'netrw', 'neo-tree', 'lazy', 'mason', 'query', 'help', 'noice', 'messages' },
     },
 }
