037b9d26ea3e7d90a0279939a668b296c574c911
Malte Esch <accounts@malteesch.de>
2024-01-08 09:04:07 +0100

    feat: use oil instead of neotree

---
+++ lua/malteesch/plugins/oil.lua
@@ -0,0 +1,26 @@
+--- @type LazyPluginSpec
+return {
+    'stevearc/oil.nvim',
+    opts = {
+        default_file_explorer = true,
+        delete_to_trash = true,
+        keymaps = {
+            q = 'actions.close',
+            H = 'actions.toggle_hidden',
+            ['?'] = 'actions.show_help',
+        },
+        view_options = {
+            show_hidden = true,
+        },
+        float = {
+            max_width = 80,
+            max_height = 25,
+        },
+    },
+    keys = {
+        -- stylua: ignore start
+        { '<leader>fo', function() require("oil").open_float() end },
+        -- stylua: ignore end
+    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
