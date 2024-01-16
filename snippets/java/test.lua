5a5d10ad5da52eade7d2dff4dafc0e154bde355b
Malte Esch <accounts@malteesch.de>
2024-01-16 08:14:07 +0100

    refactor: cleanup java test method snippet

--- snippets/java/test.lua
+++ snippets/java/test.lua
@@ -3,6 +3,23 @@ local s = ls.snippet
 local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 
+--- @return boolean
+local function is_test_class()
+    local parser = require('nvim-treesitter.parsers').get_parser()
+    local query = vim.treesitter.query.parse(parser:lang(), '(class_declaration name: (identifier) @class_name)')
+
+    local root = parser:parse()[1]:root()
+    local last_line = vim.fn.line '$' or 0
+
+    for _, node, _ in query:iter_captures(root, 0, 1, last_line) do
+        local start_row, start_col, end_row, end_col = node:range()
+        local class_name = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})[1]
+        return class_name:match 'Test'
+    end
+
+    return false
+end
+
 return {
     s(
         { trig = 'test', name = 'test method' },
@@ -14,19 +31,7 @@ void {1}() {{
             { i(1, 'test'), i(2) }
         ),
         {
-            show_condition = function()
-                local query = vim.treesitter.query.parse('java', '(class_declaration name: (identifier) @class_name)')
-                local root = vim.treesitter.get_node():tree():root()
-                local last_line = vim.fn.line('$')
-
-                for _, node, _ in query:iter_captures(root, 0, 1, last_line) do
-                    local start_row, start_col, end_row, end_col = node:range()
-                    local class_name = vim.api.nvim_buf_get_text(0, start_row, start_col, end_row, end_col, {})[1]
-                    return class_name:match 'Test'
-                end
-
-                return false
-            end,
+            show_condition = is_test_class,
         }
     ),
 }
