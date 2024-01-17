049dcd763c002063646e7f6a542dde7f6b401d79
Malte Esch <accounts@malteesch.de>
2024-01-17 08:41:30 +0100

    feat: add indent blankline again in favor of hlchunk
    also:
    update plugins

---
+++ lua/malteesch/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+--- @type LazyPluginSpec
+return {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    opts = {
+        indent = {
+            char = '▏',
+        },
+        scope = {
+            show_start = false,
+        },
+    },
+}
