9bc23e174d09b1d799b35a39a9653200890cb608
Malte Esch <accounts@malteesch.de>
2024-01-19 10:51:04 +0100

    feat: add tabby

---
+++ lua/malteesch/plugins/tabby.lua
@@ -0,0 +1,9 @@
+--- @type LazyPluginSpec
+return {
+  'nanozuki/tabby.nvim',
+  event = 'VimEnter',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function ()
+    require("tabby.tabline").use_preset('tab_only')
+  end
+}
