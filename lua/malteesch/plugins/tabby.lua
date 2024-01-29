58f13ce9154b65829c56220396233297fa6ae008
Malte Esch <accounts@malteesch.de>
2024-01-29 13:39:22 +0100

    feat: style tabby

--- lua/malteesch/plugins/tabby.lua
+++ lua/malteesch/plugins/tabby.lua
@@ -1,9 +1,30 @@
 --- @type LazyPluginSpec
 return {
-  'nanozuki/tabby.nvim',
-  event = 'VimEnter',
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function ()
-    require("tabby.tabline").use_preset('tab_only')
-  end
+    'nanozuki/tabby.nvim',
+    event = 'VimEnter',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+        local theme = {
+            fill = 'TabLineFill',
+            head = 'TabLine',
+            current_tab = 'TabLineSel',
+            tab = 'TabLine',
+        }
+        require('tabby.tabline').set(function(line)
+            return {
+                line.tabs().foreach(function(tab)
+                    local hl = tab.is_current() and theme.current_tab or theme.tab
+                    return {
+                        line.sep('', hl, theme.fill),
+                        tab.name(),
+                        line.sep('', hl, theme.fill),
+                        hl = hl,
+                        margin = ' ',
+                    }
+                end),
+                line.spacer(),
+                hl = theme.fill,
+            }
+        end)
+    end,
 }
