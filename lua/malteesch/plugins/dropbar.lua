9b1fbb4d5b3366e979adce96a23ec3388d2e86ba
Malte Esch <accounts@malteesch.de>
2024-01-30 10:27:09 +0100

    feat: add dropbar but disable it for now
    Need to read more of the docs to decide if I want it.

---
+++ lua/malteesch/plugins/dropbar.lua
@@ -0,0 +1,9 @@
+--- @type LazyPluginSpec
+return {
+    'Bekaboo/dropbar.nvim',
+    enabled = false,
+    -- optional, but required for fuzzy finder support
+    dependencies = {
+        'nvim-telescope/telescope-fzf-native.nvim',
+    },
+}
