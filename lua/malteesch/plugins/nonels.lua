a68e2265c43ead6564e698fa40d500e735bea567
Malte Esch <accounts@malteesch.de>
2024-02-26 09:22:03 +0100

    chore: remove deprecated builtin

--- lua/malteesch/plugins/nonels.lua
+++ lua/malteesch/plugins/nonels.lua
@@ -7,7 +7,6 @@ return {
         null_ls.setup {
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.formatting.rustfmt,
                 null_ls.builtins.diagnostics.yamllint
             },
         }
