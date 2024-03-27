c72de6f34cea837e9502f5cce1fc09a30c393fc5
Malte Esch <accounts@malteesch.de>
2024-03-27 15:58:25 +0100

    chore: format copilot.lua

--- lua/malteesch/plugins/copilot.lua
+++ lua/malteesch/plugins/copilot.lua
@@ -1,11 +1,11 @@
 --- @type LazyPluginSpec
 return {
     'github/copilot.vim',
-    config = function ()
+    config = function()
         vim.g.copilot_enabled = 0
-        vim.keymap.set('i', '<C-Tab>', 'copilot#Accept("\\<CR>")', {
-          expr = true,
-          replace_keycodes = false
+        vim.keymap.set({ 'n', 'i' }, '<C-Tab>', 'copilot#Accept("\\<CR>")', {
+            expr = true,
+            replace_keycodes = false,
         })
         vim.g.copilot_no_tab_map = true
     end,
