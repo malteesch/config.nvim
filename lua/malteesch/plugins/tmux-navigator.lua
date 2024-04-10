1978cce58430c72b3ba9bbe391aab6e87f076eae
Malte Esch <accounts@malteesch.de>
2024-04-10 15:26:00 +0200

    fix: fix tmux navigator overriding my keymaps lazily

--- lua/malteesch/plugins/tmux-navigator.lua
+++ lua/malteesch/plugins/tmux-navigator.lua
@@ -14,4 +14,8 @@ return {
         { '<A-k>', '<CMD>TmuxNavigateUp<CR>' },
         { '<A-l>', '<CMD>TmuxNavigateRight<CR>' },
     },
+    init = function()
+        vim.g.tmux_navigator_no_mappings = 1
+        vim.g.tmux_navigator_disable_when_zoomed = 1
+    end,
 }
